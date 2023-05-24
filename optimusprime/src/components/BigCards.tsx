3b1ad991b7c9e2fec621f22583cfbaee8229f80c
monniale <monnialessandro05@gmail.com>
2023-05-24 17:22:48 +0200

    temporary commit

--- optimusprime/src/components/BigCards.tsx
+++ optimusprime/src/components/BigCards.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+import audio from '../images/audioprova.mp3';
 
 export const BigCards = (prop:any) => {
+
+
+    const start = () => {
+        audio.play();
+    }
     return (
         <>
             <div className="BigCard">
                 <p>{prop.upperTitle1}</p>
                 <h2>{prop.title1}</h2>
                 <img src={prop.comp1}></img>
+                <button onClick={start}>Play</button>
             </div>  
             <div className="BigCard">
                 <p>{prop.upperTitle2} </p>
                 <h2>{prop.title2}</h2>
-                <ReactPlayer url={prop.comp2} controls={true} />
             </div>
         </>
     )
