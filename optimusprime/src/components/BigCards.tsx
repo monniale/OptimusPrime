0befec6556a9b37b6f60a299966b2aedd2537ca1
monniale <monnialessandro05@gmail.com>
2023-05-31 17:25:50 +0200

    fix: video-monni changed

--- optimusprime/src/components/BigCards.tsx
+++ optimusprime/src/components/BigCards.tsx
@@ -1,77 +1,87 @@
-import React from 'react';
-import video1 from '../images/monnii.mp4';
+import React from "react";
+import videomonni from "../images/video-monni.mp4";
 
 const blackCard = {
-    backgroundColor: '#000',
-    color: '#fff',
-}
+  backgroundColor: "#000",
+  color: "#fff",
+};
 
 const whiteCard = {
-    backgroundColor: '#fff',
-    color: '#000',
-}
+  backgroundColor: "#fff",
+  color: "#000",
+};
 
 let alreadyClicked = false;
-function startVideo(index:any) {
-    const video = document.getElementsByTagName('video')[index];
-    if (alreadyClicked) {
-        video.pause();
-        alreadyClicked = !alreadyClicked;
-    } else {
-        video.play();
-        alreadyClicked = !alreadyClicked;
-    }
+function startVideo(index: any) {
+  const video = document.getElementsByTagName("video")[index];
+  if (alreadyClicked) {
+    video.pause();
+    alreadyClicked = !alreadyClicked;
+  } else {
+    video.play();
+    alreadyClicked = !alreadyClicked;
+  }
 }
 
-function restartVideo(index:any) {
-    const video = document.getElementsByTagName('video')[index];
-    video.currentTime = 0;
+function restartVideo(index: any) {
+  const video = document.getElementsByTagName("video")[index];
+  video.currentTime = 0;
 }
 
-
-export const BigCards = (prop:any) => {
-    if (prop.order == 0){
+export const BigCards = (prop: any) => {
+  if (prop.order == 0) {
     return (
-        <>
-            <div className="CardContainer">
-            <div className="BigCard" style={whiteCard}>
-                <p>{prop.upperTitle1}</p>
-                <h2>{prop.title1}</h2>
-                <p>{prop.comp1}</p>
-            </div>  
-            <div className="BigCard" style={blackCard}>
-                <p>{prop.upperTitle2} </p>
-                <h2>{prop.title2}</h2>
-                <div className="VideoContainer">
-                <video controls={false} autoPlay  onClick={() => startVideo(prop.indiceVideo)} onDoubleClick={() => restartVideo(prop.indiceVideo)}>
-                    <source type="video/mp4" src={video1} />
-                    Your browser does not support the video element.
-                </video>
-      </div>
+      <>
+        <div className="CardContainer">
+          <div className="BigCard" style={whiteCard}>
+            <p>{prop.upperTitle1}</p>
+            <h2>{prop.title1}</h2>
+            <p>{prop.comp1}</p>
+          </div>
+          <div className="BigCard" style={blackCard}>
+            <p>{prop.upperTitle2} </p>
+            <h2>{prop.title2}</h2>
+            <div className="VideoContainer">
+              <video
+                controls={false}
+                autoPlay
+                onClick={() => startVideo(prop.indiceVideo)}
+                onDoubleClick={() => restartVideo(prop.indiceVideo)}
+              >
+                <source type="video/mp4" src={videomonni} />
+                Your browser does not support the video element.
+              </video>
             </div>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="CardContainer">
+          <div className="BigCard" style={blackCard}>
+            <p>{prop.upperTitle1}</p>
+            <h2>{prop.title1}</h2>
+            <div className="VideoContainer">
+              <video
+                controls={false}
+                autoPlay
+                onClick={() => startVideo(prop.indiceVideo)}
+                onDoubleClick={() => restartVideo(prop.indiceVideo)}
+              >
+                <source type="video/mp4" src={videomonni} />
+                Your browser does not support the video element.
+              </video>
             </div>
-        </>
-    )} else {
-        return (
-            <>
-            <div className="CardContainer">
-            <div className="BigCard" style={blackCard}>
-                <p>{prop.upperTitle1}</p>
-                <h2>{prop.title1}</h2>
-                <div className="VideoContainer">
-                <video controls={false} autoPlay  onClick={() => startVideo(prop.indiceVideo)} onDoubleClick={() => restartVideo(prop.indiceVideo)}>
-                    <source type="video/mp4" src={video1} />
-                    Your browser does not support the video element.
-                </video>
-      </div>
-            </div>  
-            <div className="BigCard" style={whiteCard}>
-                <p>{prop.upperTitle2} </p>
-                <h2>{prop.title2}</h2>
-                <p>{prop.comp2}</p>
-            </div>
-            </div>
-        </>
-    )
-
-}};
+          </div>
+          <div className="BigCard" style={whiteCard}>
+            <p>{prop.upperTitle2} </p>
+            <h2>{prop.title2}</h2>
+            <p>{prop.comp2}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+};
