730fa792525252470015cb4392a48ef4cf8636ea
monniale <monnialessandro05@gmail.com>
2023-06-01 18:26:19 +0200

    feat: video published

--- optimusprime/src/components/Team.tsx
+++ optimusprime/src/components/Team.tsx
@@ -4,30 +4,29 @@ import dainese from "../images/dainese.png";
 import dallamuta from "../images/dallamuta.png";
 
 export const Team = () => {
-  
   return (
     <>
       <div className="Team">
         <div className="row">
           <div className="column">
-            <img src={benve} alt="" className="TeamImage"/>
+            <img src={benve} alt="" className="TeamImage" />
             <p>Andrea Benvegnù</p>
             <p className="SubTeam">ReactDev</p>
           </div>
           <div className="column">
-            <img src={monni} alt="" className="TeamImage Monni"/>
+            <img src={monni} alt="" className="TeamImage Monni" />
             <p>Monni Alessandro</p>
             <p className="SubTeam">ReactDev</p>
           </div>
           <div className="column">
-            <img src={dallamuta} alt="" className="TeamImage"/>
-            <p>Dalla Muta Thomas</p>
-            <p className="SubTeam">Math Expert</p>
-          </div>
-          <div className="column">
-            <img src={dainese} alt="" className="TeamImage"/>
+            <img src={dainese} alt="" className="TeamImage" />
             <p>Dainese Riccardo</p>
             <p className="SubTeam">Keynote Manager</p>
+          </div>
+          <div className="column">
+            <img src={dallamuta} alt="" className="TeamImage" />
+            <p>Dalla Muta Thomas</p>
+            <p className="SubTeam">Math Expert</p>
           </div>
         </div>
       </div>
